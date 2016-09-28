93905cd5b11eef2cf46131bcf4d7c5df40363089
Cole Palm <palm.cole@gmail.com>
2016-09-28 16:46:41 -0700

    create

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-console.log('blah')
+const express = require('express');
+const app = express();
 
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html')
+})
+
+app.post('/people', (request, response) => {
+  console.log("Person has been added")
+})
