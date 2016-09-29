ef34311a18909b22bcb8f2142e50417874b752d1
Cole Palm <palm.cole@gmail.com>
2016-09-28 17:04:35 -0700

    create with db

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+var db
+
+MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds023530.mlab.com:23530/crud-people', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
 })
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html')
 })
 
 app.post('/people', (request, response) => {
-  console.log("Person has been added")
+  db.collection('people').save(req.body, (err, result)) => {
+    if (err) return console.log(err)
+
+    console.log('post complete')
+    res.redirect('/')
+  }
 })
